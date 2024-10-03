8296663051212738ddcd53ead4913c9a7555cae9
Aum Shukla <aum.shukla@wsl.com>
2024-10-03 18:45:25 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,84 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import aveshdarbar from "./aveshdarbar.png";
+import raj from "./raj.png";
+import rajashree from "./rajashree.png"
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import Begin from "./company/Begin.png";
+import collabera from "./company/collabera.png";
+import Rishabhsoftware from "./company/Rishabhsoftware.png"
+import Goodspace from "./company/Goodspace.png";
+import patterns from "./company/patterns.png";
+import Skilltelligent from "./company/Skilltelligent.png";
+import windowmaker from "./company/windowmaker.png";
+import ifortis from "./company/ifortis.png";
+ 
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  Begin,
+  collabera,
+  Goodspace,
+  patterns,
+  Rishabhsoftware,
+  Skilltelligent,
+  windowmaker,
+  ifortis,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  aveshdarbar,
+  raj,
+  rajashree
+};
---
+++ src/constants/index.js
@@ -0,0 +1,314 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  Rishabhsoftware,
+  Begin,
+  collabera,
+  Goodspace,
+  patterns,
+  Skilltelligent,
+  windowmaker,
+  ifortis,
+  carrent,
+  jobit,
+  tripguide,
+  aveshdarbar,
+  raj,
+  rajashree,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Web Developer",
+    company_name: "Windowmaker Software Ltd · Full-time",
+    icon: windowmaker,
+    iconBg: "#ffffff",
+    date: "May 2024 - Present",
+    points: [
+      "Responsible for designing, developing, and maintaining the company's web applications.",
+      "My tasks includes writing clean, efficient code, implementing responsive design, and optimizing websites for performance.",
+      "Work with the design and backend both to ensure seamless integration of UI/UX elements, troubleshoot issues, and enhance site functionality.",
+      "Focusing on improving the overall user experience and ensuring that web solutions meet business requirements while staying updated with the latest web development trends and technologies.",
+    ],
+  },
+  {
+    title: "Customer Service Specialist",
+    company_name: "PATTERNS LLC",
+    icon: patterns,
+    iconBg: "#ffffff",
+    date: "Dec 2023 - Apr 2024 ",
+    points: [
+      "Was responsible for providing exceptional support to customers by addressing their inquiries, resolving issues, and ensuring a positive experience.",
+      "Handling customer interactions through various channels, such as phone, email, and chat, while maintaining a high level of professionalism and empathy. ",
+      "Assisted in troubleshooting product or service issues, processed orders, and provided solutions to enhance customer satisfaction. ",
+    ],
+  },
+  {
+    title: "SQL Developer Intern",
+    company_name: "Collabera Digital",
+    icon: collabera,
+    iconBg: "#000000",
+    date: "Aug 2023 - Nov 2023",
+    points: [
+      "Was responsible for writing and optimizing SQL queries to manage and analyze data across various databases.",
+      "Involved in designing and maintaining database schemas, ensuring data integrity, and assisting in the development of data-driven applications. ",
+      "Collaborated with the development team to troubleshoot and optimize database performance, created reports for business analysis, and supported data migration tasks. ",
+    ],
+  },
+  {
+    title: "Sales And Marketing Executive",
+    company_name: "GoodSpace",
+    icon: Goodspace,
+    iconBg: "#ffffff",
+    date: "Oct 2022 - Jan 2023",
+    points: [
+      "Played a key role in developing and executing strategies to drive both sales and brand visibility. Your responsibilities included identifying potential clients, nurturing relationships, and managing the sales pipeline to ensure growth.",
+      "Involved in crafting and delivering marketing campaigns designed to attract new customers while maintaining engagement with existing ones.",
+      "Analyzing market trends and customer feedback, you continuously refined your approach to meet targets and contribute to the overall success of the company’s marketing efforts.",
+    ],
+  },
+  {
+    title: "Sales Marketing Executive - Internship",
+    company_name: "Begin",
+    icon: Begin,
+    iconBg: "#00008B",
+    date: "Aug 2022 - Sep 2022",
+    points: [
+      "Was responsible for driving sales growth and implementing marketing strategies to promote the company's products or services.",
+      "Identifying potential clients, conducting market research, and developing sales pitches to attract new business.",
+      "Collaborated with the marketing team to design promotional campaigns, manage customer relationships, and monitor market trends to adjust strategies accordingly.",
+    ],
+  },
+  {
+    title: "Corporate Ambassador - Internship",
+    company_name: "IFORTIS WORLDWIDE™",
+    icon: ifortis,
+    iconBg: "#000000",
+    date: "Jun 2022 - Aug 2022",
+    points: [
+      "Was responsible for promoting the company's brand and services, both online and offline.",
+      "Collaborated with the marketing team to enhance brand visibility and participated in strategic discussions to improve engagement with corporate clients.",
+      "Building relationships with potential clients, representing the company at various events, and driving business development initiatives.",
+      "Assisted in outreach efforts, generating leads, and creating awareness about the company's offerings.",
+    ],
+  },
+  {
+    title: "Cloud Engineer - Internship",
+    company_name: "Rishabh Software",
+    icon: Rishabhsoftware,
+    iconBg: "#ffffff",
+    date: "Jan 2022 - Jun 2022",
+    points: [
+      "Was involved in designing, deploying, and maintaining cloud infrastructure.",
+      "My responsibilities included assisting in the configuration of cloud environments, optimizing cloud solutions for performance, and ensuring scalability and security.",
+      "Also worked on automating cloud operations, supporting cloud migrations, and collaborating with senior engineers to troubleshoot and resolve issues in cloud-based applications and services.",
+      "Hands-on experience helped you gain expertise in cloud platforms like AWS, Azure, or Google Cloud",
+    ],
+  },
+  {
+    title: "Digital Marketing Executive - Internship",
+    company_name: "SkillTelligent Solutions Pvt. Ltd.",
+    icon: Skilltelligent,
+    iconBg: "#ffffff",
+    date: "Mar 2020 - Jun 2020",
+    points: [
+      "Was responsible for executing and managing digital marketing campaigns across various online platforms.",
+      "Developing strategies to increase brand awareness, generating leads, and improving customer engagement. You managed social media accounts, optimized content for search engines (SEO), ran paid advertising campaigns (PPC), and analyzed campaign performance through metrics and data analytics.",
+      "Collaborated with the content team to create compelling digital content and ensured the brand’s online presence aligned with marketing goals..",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I wholeheartedly recommend Aum Shukla. His outstanding grasping power, eagerness to learn, and strong technical skills set him apart. A quick learner with a keen interest in mastering complex concepts, Aum's contributions to any academic or professional setting would be invaluable. He brings a unique blend of intellect and enthusiasm, making him a standout candidate.",
+    name: "Avesh Gaur",
+    designation: "Assosiate Software Developer",
+    company: "atQor (formerly bitscape)",
+    image: aveshdarbar,
+  },
+  {
+    testimonial:
+      "Aum is a very disciplined and dedicated professional. He can handle any problem and situation with ease and confidence..He is sincere and gives hundred percentage to any task given to him and believes in time management too.I loved working with him and can say that he will be an asset for any company he works with. Had a great time working with Aum with new outcomes and challanges",
+    name: "Raj Chhadva",
+    designation: "COO",
+    company: "FuzeMee",
+    image: raj,
+  },
+  {
+    testimonial:
+      "Aum is one of the most dedicated professionals I've worked with. His ability to to tackle any problem is remarkable and with a warm smile. He is a very sincere and obedient team member. I have loved my experience working with him. He will be an asset for the company he is hired for. Well done keep the working spirit on just like you do regularly",
+    name: "Rajashree Roy",
+    designation: "HR",
+    company: "Lernx",
+    image: rajashree,
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rental System",
+    description:
+      "It enables efficient management of vehicle reservations, customer profiles, billing, and maintenance scheduling. Key features include a user-friendly booking interface, real-time vehicle availability tracking, payment processing, and automated notifications for rentals and returns.The system also generates reports on fleet utilization and customer preferences.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/Aum64/Car-Rental.git",
+  },
+  {
+    name: "House Price Prediction",
+    description:
+      "Built using Python, the system leverages historical data on house prices along with features like location, square footage, number of rooms, and amenities to forecast future property values. Using algorithms such as Linear Regression, Decision Trees, and Random Forest, the system provides accurate price predictions.This tool is valuable for real estate agents.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/Aum64/houserate-prediction.git",
+  },
+  {
+    name: "Stock Market Prediction",
+    description:
+      "It analyzes historical stock data, market trends, and financial indicators to predict stock movements. The system leverages models like Linear Regression, LSTM, and Random Forest to provide short-term and long-term stock price predictions. This tool helps investors and traders make informed decisions by identifying potential market trends and investment opportunities.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/Aum64/Stockmarket-Prediction.git",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
